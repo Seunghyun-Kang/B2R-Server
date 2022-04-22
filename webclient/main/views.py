d2e91b4c932f97642034eadeeba19bfbfde76a11
Seunghyun-Kang <kiseyno@gmail.com>
2022-04-22 23:18:20 +0900

    Fix typo issue in views.py

--- webclient/main/views.py
+++ webclient/main/views.py
@@ -267,10 +267,7 @@ def getLastBollingerReverseSignal(request, symbol, lastday):
 def getMomentum(request, symbol, duration):
     try:
         partial_hash = str(symbol) + '_' + str(30) + '_' + str(duration)
-        print("@@@@@@@@@@@@")
-        print(partial_hash)
-        queryset = Momentum.objects.filter(hashcode__icontains=partial_hash)
-        print(len(queryset))
+        info = Momentum.objects.filter(hashcode__icontains=partial_hash)
         serializer = MomentumSerializer(info, many=True)
     except:
         return Response(status=status.HTTP_404_NOT_FOUND)
