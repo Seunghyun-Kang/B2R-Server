fce67f6d045c616611fda36d7682046742bba1b3
Seunghyun-Kang <kiseyno@gmail.com>
2023-01-19 22:31:58 +0900

    Add scheduler in app.py

--- webclient/main/apps.py
+++ webclient/main/apps.py
@@ -1,6 +1,11 @@
 from django.apps import AppConfig
-
+from django.conf import settings
 
 class MainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'main'
+
+    def ready(self):
+        if settings.SCHEDULER_DEFAULT:
+            from .scheduler import start
+            start()
